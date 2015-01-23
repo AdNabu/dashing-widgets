888875003b4e625b7436cf3a6553541323cfceb1
salil <salilpa007@gmail.com>
2015-01-23 19:12:57 +0530

    Adding info to the widget

--- widgets.py
+++ widgets.py
@@ -7,13 +7,15 @@ class UserCards(NumberWidget):
     title = 'New users'
 
     def get_more_info(self):
-        return '500 open'
+        today = datetime.today()
+        first_day_of_month = datetime(today.year, today.month, 1)
+        return '%d users this month' % User.objects.exclude(date_joined__lt=first_day_of_month).count()
 
     def get_updated_at(self):
         return 'updated at ' + datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
     def get_value(self):
-        return '%d' % User.objects.exclude(date_joined__gt=datetime.today()-timedelta(days=7)).count()
+        return '%d' % User.objects.exclude(date_joined__lt=datetime.today()-timedelta(days=7)).count()
 
     def get_detail(self):
         return '%d total users' % User.objects.count()
